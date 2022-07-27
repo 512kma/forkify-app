d54f3b66d58bd70c8f2659a8c295a2013ecee828
512kma <kane_ma@outlook.com>
2022-07-27 10:19:52 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,6 +10,7 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
+import features from 'core-js/features';
 
 const controlRecipes = async function () {
   try {
@@ -119,6 +120,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the Application!");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -127,5 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
